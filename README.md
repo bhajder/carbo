8102a3ae08d257b4f88aea76c025608238a412af
bhajder <bhajder@mobitouch.net>
2020-06-25 12:41:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+CART - Clean Atomic React Toolkit
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Clean Atomic React Toolkit
+    </div>
+  );
+}
+
+export default App;
